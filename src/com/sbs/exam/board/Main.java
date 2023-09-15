a6f57e719a3a795dba3ad2a8fd50077a9d806ba3
SangWon7242 <gotkdqja@gmail.com>
2023-09-15 19:38:40 +0900

    C:/Program Files/Git/usr/article/detail 명령어로 최근 게시물 보기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -2,10 +2,17 @@ package com.sbs.exam.board;
 
 import java.util.Scanner;
 
+/*
+Todo 예시
+- [o] /usr/article/detail 입력처리
+- [] /usr/article/detail 입력되면 가장 최근 게시물 보여주기
+*/
+
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("== 프로그램 시작 ==");
@@ -26,16 +33,28 @@ public class Main {
         articleLastId = id;
 
         Article article = new Article(id, title, content);
+        lastArticle = article;
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
         System.out.println("생성된 게시물 객체 : " + article);
       }
+      if(cmd.equals("/usr/article/detail")) {
+
+        if(lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = lastArticle;
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.content);
+      }
       else if(cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
-      }
-      else {
-        System.out.println("명령어를 잘못 입력 하셨습니다.");
       }
     }
 
